008cc2e23e0b1ed6aa4c12c5cf45b5b053f8f30f
AgomohC <chinaemerem98@yahoo.com>
2021-08-06 19:11:39 +0100

    fixed the styling of the placeholder text in searchbar.js

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -8,19 +8,14 @@ const Searchbar = () => {
     <section className="container">
       <div className="row justify-content-center d-flex">
         <form className="col-12">
-          <div className="form-group mx-auto col-12 col-md-8 col-lg-6 bg-light mt-5 p-3  shadow rounded d-flex justify-content-center">
-            <label
-              htmlFor="searchInput"
-              className="text-secondary text-capitalize  my-auto col-2 pr-2 mr-3 mr-md-auto"
-            >
-              search
-            </label>
+          <div className="form-group mx-auto col-12 col-md-8 col-lg-6 bg-light mt-5 p-3 shadow rounded d-flex justify-content-center">
             <input
               type="text"
               id="searchInput"
               value={searchValue}
               onChange={(e) => searchbarFunction(e)}
-              className="form-control text-light bg-secondary mx-auto width-400 my-auto pl-2 col-10"
+              className="form-control text-light bg-secondary mx-auto width-400 my-auto col-12"
+              placeholder="search by title"
             />
           </div>
         </form>
