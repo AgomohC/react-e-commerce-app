66f64a368434beb177a77e07669287dcc2d9367d
AgomohC <chinaemerem98@yahoo.com>
2021-08-14 19:55:42 +0100

    reinstalled firebase

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,7 +1,7 @@
 import React from "react";
-
 import authReducer from "../reducers/authReducer";
 import { useContext, useReducer } from "react";
+import { auth } from "../firebase";
 
 const AuthContext = React.createContext();
 
